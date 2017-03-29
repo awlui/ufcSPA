42b1aff01bfd8651aa5142469c273b70965a158a
Andy Lui <andylui@Andys-MBP.home>
2017-03-28 18:05:24 -0700

    fighter search improved

--- src/javascripts/angularApp/config/routes.js
+++ src/javascripts/angularApp/config/routes.js
@@ -7,11 +7,31 @@ angular.module('ufcApp')
   	//Fighters Page
   	$routeProvider.when('/Fighters', {
   		templateUrl: "src/templates/fighterList.html",
-  		controller: ['fighterList', function(fighterList) {
-        this.options = {
-          weight_class: 'Featherweight'
-        };
-  			this.fighterList = fighterList.data;
+  		controller: ['fighterList', 'fighterSearchService', function(fighterList, fighterSearchService) {
+        var self = this;
+        self.options = function() {
+          var flag = false;
+          $('optgroup').css('display', 'none');
+          for (key in self.weightClass) {
+            if (self.weightClass[key] === true) {
+              $('optgroup[label="' + key + '"]').css('display', 'block');
+              flag = true;
+            } else {
+              $('optgroup[label="' + key + '"]').css('display', 'none');
+            }
+          }
+          if (flag === false) {
+            $('optgroup').css('display', 'block');
+          }
+        }
+  			self.fighterList = fighterList.data;
+
+        self.fighterSearch = function() {
+          fighterSearchService.query(self.currentFighterID).then(function(data) {
+            self.currentFighter = data.data;
+          });
+
+        }
 
   		}],
       controllerAs: 'mainCtrl',
@@ -24,15 +44,23 @@ angular.module('ufcApp')
   	});
   }]);
 
-  angular.module('ufcApp')
-    .filter('nullWeightClass', [function() {
-      return function(fighterList) {
-        var array = [];
-        angular.forEach(fighterList, function(fighter) {
-          if (fighter.weight_class !== null) {
-            array.push(fighter);
-          }
-        });
-        return array;
+angular.module('ufcApp')
+  .filter('nullWeightClass', [function() {
+    return function(fighterList) {
+      var array = [];
+      angular.forEach(fighterList, function(fighter) {
+        if (fighter.weight_class !== null) {
+          array.push(fighter);
+        }
+      });
+      return array;
+    }
+  }]);
+angular.module('ufcApp')
+  .factory('fighterSearchService', ['$http', function($http) {
+    return {
+      query: function(id) {
+        return $http.get('http://localhost:3000/api/fighters/' + id);
       }
-    }])
+    }
+  }]);
