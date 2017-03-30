f456af94dffbbc9ab2a994a0b71470429f36bfbe
Andy Lui <andylui@Andys-MBP.home>
2017-03-29 18:32:12 -0700

    Fighter info feature finished

--- src/javascripts/angularApp/filters/index.js
+++ src/javascripts/angularApp/filters/index.js
@@ -53,3 +53,14 @@ angular.module('ufcApp')
       return array;
     }
   }]);
+
+angular.module('ufcApp')
+  .filter('outcomeFilter', [function() {
+    return function(outcome) {
+      if (outcome === null) {
+        return "Upcoming";
+      } else {
+        return outcome;
+      }
+    }
+  }])
