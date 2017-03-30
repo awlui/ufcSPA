d53099a9e387916f827d6ac6257e9482e27cba14
Andy Lui <andylui@Andys-MBP.home>
2017-03-29 18:37:20 -0700

    swap api urls

--- src/javascripts/angularApp/services/index.js
+++ src/javascripts/angularApp/services/index.js
@@ -2,7 +2,7 @@ angular.module('ufcApp')
   .factory('fighterSearchService', ['$http', function($http) {
     return {
       query: function(id) {
-        return $http.get('http://localhost:3000/api/fighters/' + id);
+        return $http.get('https://gentle-citadel-87711.herokuapp.com/api/fighters/' + id);
       }
     }
   }]);
