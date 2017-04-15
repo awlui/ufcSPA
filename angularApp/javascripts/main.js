9f6d27edceb0dedf8637dd0c38fffcd03309cd31
Andy Lui <andylui@Andys-MBP.home>
2017-04-15 00:32:18 -0700

    remove navbar on scroll down; improve ux for mobile users drastically

--- angularApp/javascripts/main.js
+++ angularApp/javascripts/main.js
@@ -1,11 +1,22 @@
-// $(function() {
-// 	$('button').on('click', function() {
-// 	$('.carousel').slick({
-// 		// lazyLoad: 'ondemand',
-// 		infinite: true,
-// 		autoplay: true,
-// 		autoplaySpeed: 2000,
-
-// 	}); 
-// 	})
-// });
+(function(window, $) {
+    var previousScroll = 0;
+    var headerOrgOffset = $('nav').height();
+    var foo =function() {
+        var currentScroll = $(this).scrollTop();
+        if (currentScroll > headerOrgOffset) {
+            if (currentScroll > previousScroll) {
+                $('nav').fadeOut();
+            } else {
+                $('nav').fadeIn();
+            }
+        }
+        previousScroll = currentScroll;
+    }
+    if ($('body').width() < 1000) {
+    $('body').on({
+        'touchmove': foo
+    });
+    } else {
+    $(window).scroll(foo);
+    }
+})(window, $);
--- public/mainApp.js
+++ public/mainApp.js
@@ -430,17 +430,29 @@ angular.module('ufcApp')
   		split: split
   	}
   }]);
-// $(function() {
-// 	$('button').on('click', function() {
-// 	$('.carousel').slick({
-// 		// lazyLoad: 'ondemand',
-// 		infinite: true,
-// 		autoplay: true,
-// 		autoplaySpeed: 2000,
+(function(window, $) {
+    var previousScroll = 0;
+    var headerOrgOffset = $('nav').height();
+    var foo =function() {
+        var currentScroll = $(this).scrollTop();
+        if (currentScroll > headerOrgOffset) {
+            if (currentScroll > previousScroll) {
+                $('nav').fadeOut();
+            } else {
+                $('nav').fadeIn();
+            }
+        }
+        previousScroll = currentScroll;
+    }
+    if ($('body').width() < 1000) {
+    $('body').on({
+        'touchmove': foo
+    });
+    } else {
+    $(window).scroll(foo);
+    }
+})(window, $);
 
-// 	}); 
-// 	})
-// });
 angular.module('ufcApp')
   .filter('locationFilter', [function() {
     return function(arr) {
