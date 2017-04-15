306e686c388aab6db2a4b259d273d539d608b1fd
Andy Lui <andylui@Andys-MBP.home>
2017-04-14 23:23:46 -0700

    cors support

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ var port = process.env.PORT || 3000;
 
 app.set('port', port);
 app.use(logger('dev'));
-
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+});
 app.get('/', function(req, res, next) {
 	res.sendFile(path.join(__dirname, 'angularApp', 'index.html'));
 });
